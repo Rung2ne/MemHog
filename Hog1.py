6dfdb3e7c2d9741aab203deb138b6ed072159476
Yoguter12 <174098611+Rung2ne@users.noreply.github.com>
2026-05-11 10:30:12 +0900

    Update Hog1.py

--- Hog1.py
+++ Hog1.py
@@ -3,17 +3,17 @@ import time
 memory_hog = []
 count = 0
 
-print("⚠️ Start ⚠️\n")
+print("⚠️메모리 점유 테스트 시작⚠️\n")
 
 try:
     while True:
         memory_hog.append(' ' * 10**6)
         count += 1
 
-        print(f"\rMemory Used: About {count} MB", end="")
+        print(f"\r현재 갉아먹은 메모리: 약 {count} MB", end="")
         time.sleep(0.01)
 
 except KeyboardInterrupt:
-    print(f"\n\n🛑 Stopped. Memory Used: About {count} MB")
+    print(f"\n\n🛑 테스트가 중지되었습니다. 최종 할당량: 약 {count} MB")
     memory_hog.clear()
-    print("Cleaned")
+    print("메모리를 성공적으로 비웠습니다.")
